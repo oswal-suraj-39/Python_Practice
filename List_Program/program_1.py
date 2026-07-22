f0fd1c9e69132d963757468fa4840dd431a4b9d6
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-22 22:03:26 +0530

    refactor: improve input handling and duplicate removal logic

--- List_Program/program_1.py
+++ List_Program/program_1.py
@@ -1,3 +1,9 @@
 # Write a Python program to accept n numbers in list and remove duplicates from a list.
 N = int(input("enter how many number: "))
 numbers = []
+print("---enter the number---")
+for i in range(N):
+    num = int(input())
+    if num not in numbers:
+        numbers.append(num)
+print("list without duplicate: ", numbers)
