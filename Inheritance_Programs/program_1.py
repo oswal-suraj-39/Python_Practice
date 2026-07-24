c1f69e8d53e1abda3ef80c13818659b3c3d999cc
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-24 22:37:25 +0530

    add Employee class with methods for accepting and displaying employee details

--- Inheritance_Programs/program_1.py
+++ Inheritance_Programs/program_1.py
@@ -3,3 +3,23 @@
 # Define methods accept and display in both the classes. 
 # Create n objects of the manager class and 
 # display the details of the manager having the maximum total salary (salary+bonus).
+
+class Employee:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.department = ""
+        self.salary = 0
+
+    def accept(self):
+        self.id = int(input("Enter employee id: "))
+        self.name = input("Enter employee name: ")
+        self.department = input("Enter department name: ")
+        self.salary = int(input("Enter salary: "))
+
+    def display(self):
+        print("----- Employee Details -----")
+        print("Employee ID: ",self.id)
+        print("Employee Name: ",self.name)
+        print("Department Name: ",self.department)
+        print("Salary: ",self.salary)
