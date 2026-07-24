83527aa9e1c1bd9065c7b3d292564cb994f3c90d
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-24 22:42:16 +0530

    add Strings class with methods to get and print a string in uppercase

--- Class_Programs/program_1.py
+++ Class_Programs/program_1.py
@@ -1,2 +1,9 @@
 # Write a Python script using class, which has two methods get_String and print_String.
-# get_String accept a string from the user and print_String print the string in upper case. 
+# get_String accept a string from the user and print_String print the string in upper case.
+
+class Strings:
+    def get_String(self):
+        self.string = input("enter a string: ")
+    
+    def print_String(self):
+        print(self.string.upper())
