2c07d8f684a8872c6f986cf4a0bc1ffc059190d0
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-24 22:36:23 +0530

    add initial implementation of Employee and Manager classes with methods for data handling

---
+++ Inheritance_Programs/program_1.py
@@ -0,0 +1,5 @@
+# Define a class Employee having members id, name, department, salary. 
+# Create a subclass called manager with member bonus. 
+# Define methods accept and display in both the classes. 
+# Create n objects of the manager class and 
+# display the details of the manager having the maximum total salary (salary+bonus).
