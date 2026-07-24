315f31fb84b4826fa1a91c8aa10488be698eb617
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-24 22:56:45 +0530

    add function to check and replace key/value pairs in a dictionary

--- Dictionary_Program/program_1.py
+++ Dictionary_Program/program_1.py
@@ -1,2 +1,13 @@
 # Write a Python program to check if a given key already exists in a dictionary.
 # If key exists replace with another key/value pair.
+
+def isKey (di, key):
+    if key in di:
+        print("key is exist")
+        del di[key]
+        newKey = int(input("enter new key number: "))
+        newValue = input("enter a value string: ")
+        di[newKey] = newValue
+        print(di)
+    else:
+        print("key is not exist")
