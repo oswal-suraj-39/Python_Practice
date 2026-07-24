cddf9deafa08c60a94914826b0c16f0e4a6f7be8
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-24 23:01:18 +0530

    add Student class definition with constructor for roll number, name, age, and gender

--- Inheritance_Programs/program_2.py
+++ Inheritance_Programs/program_2.py
@@ -1,3 +1,10 @@
 # Write a python script to define a class student having members roll no, name, age, gender.
 # Create a subclass called Test with member marks of 3 subjects. 
 # Create three objects of the Test class and display all the details of the student with total marks. 
+
+class Student:
+    def __init__(self, r, n, a, g):
+        self.rollno = r
+        self.name = n
+        self.age = a
+        self.gender = g
