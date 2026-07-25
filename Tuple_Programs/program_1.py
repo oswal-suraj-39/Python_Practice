123ca48f3aed6a59a3173d9adc6076573f442222
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-25 23:16:08 +0530

    add: implement script to count repeated items in a tuple by for-in loop and print element and count

--- Tuple_Programs/program_1.py
+++ Tuple_Programs/program_1.py
@@ -1,3 +1,13 @@
 # Write a python script to find the repeated items of a tuple
+
 tuple1 = (2, 4, 5, 6, 2, 3, 4, 4, 7)
 dict1 = {}
+
+for i in tuple1:
+    if i in dict1:
+        dict1[i] += 1
+    else:
+        dict1[i] = 1
+        
+print("elements: ", tuple(dict1.keys()))
+print("count   : ", tuple(dict1.values()))
