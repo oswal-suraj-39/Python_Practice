af5c5111641190f2d275caa9c52f055792a0e9e4
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-25 23:22:38 +0530

    add: implement class with methods to get, print, and reverse a string

--- String_Programs/program_2.py
+++ String_Programs/program_2.py
@@ -1,3 +1,19 @@
 # Write a Python class which has two methods get_String and print_String. 
 # get_String accept a string from the user and print_String print the string in upper case. 
 # Further modify the program to reverse a string word by word and print it in lower case.
+
+class Strings:
+    def get_String(self):
+        self.string = input("enter a string: ")
+    
+    def print_String(self):
+        print(self.string.upper())
+
+    def reverse_String (self):
+        reverse_str = self.string[::-1]
+        print(reverse_str.lower())
+        
+objStr = Strings()
+objStr.get_String()
+objStr.print_String()
+objStr.reverse_String()
