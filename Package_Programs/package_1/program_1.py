d640d4032088af211723aed8c0bcee55a67ea437
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-25 23:10:42 +0530

    print output for area and volume calculations

--- Package_Programs/package_1/program_1.py
+++ Package_Programs/package_1/program_1.py
@@ -4,3 +4,8 @@ import cude as c
 import sphere as s
 
 num = int(input("enter a number: "))
+
+print("area of cube: ", c.area(num))
+print("volume of cube: ", c.volume(num))
+print("area of sphere: ", s.area(num))
+print("volume of sphere: ", s.volume(num))
