1f659996abb4079db64c84acfbb81a56a67537be
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-25 23:09:38 +0530

    add scripts to calculate area and volume of cube and sphere in package_1

---
+++ Package_Programs/package_1/cude.py
@@ -0,0 +1,7 @@
+# Write python script using package to calculate area and volume of cube and sphere
+
+def area (side):
+    return 6 * side * side
+
+def volume (side):
+    return side * side * side
---
+++ Package_Programs/package_1/sphere.py
@@ -0,0 +1,7 @@
+# Write python script using package to calculate area and volume of cube and sphere
+
+def area (r):
+    return 4 * 3.14 * r * r
+
+def volume (r):
+    return (4/3) * 3.14 * r * r * r
