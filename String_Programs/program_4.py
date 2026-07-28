0d9f6b0537b6ad8f8fef356f186b90581bd45f8d
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-28 19:12:23 +0530

    add: implement character counting in a string

--- String_Programs/program_4.py
+++ String_Programs/program_4.py
@@ -1,3 +1,15 @@
 # Write a python program to count repeated characters in a string.
 # Sample string: 'thequickbrownfoxjumpsoverthelazydog'
 # Expected output: o-4, e-3, u-2, h-2, r-2, t-2
+
+string = input("enter a string: ")
+dict1 = {}
+
+for i in list(string):
+    if i in dict1:
+        dict1[i] += 1
+    else:
+        dict1[i] = 1
+        
+for k, v in dict1.items():
+    print(k, " - ", v, end=", ")
