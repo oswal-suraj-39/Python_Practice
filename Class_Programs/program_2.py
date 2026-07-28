e0447b9a8700777a99020a65f31c062ab058479f
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-28 19:00:04 +0530

    add: implement BracketValidator class to check validity of parentheses

--- Class_Programs/program_2.py
+++ Class_Programs/program_2.py
@@ -1,3 +1,24 @@
 # Write a Python class to find validity of a string of parentheses, '(', ')', '{', '}', '['  ']’. 
 # These brackets must be close in the correct order. for example "()" and "()[]{}" are valid 
 # but "[)", "({[)]" and "{{{" are invalid. i maybe understand
+
+class BracketValidator:
+    def __init__(self, string):
+        self.string = string
+
+    def isValid(self):
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+
+        for char in self.string:
+            if char in bracket_map.values():  # opening brackets
+                stack.append(char)
+            elif char in bracket_map:  # closing brackets
+                if not stack or stack[-1] != bracket_map[char]:
+                    return False
+                stack.pop()
+            else:
+                # Invalid character (optional to allow or disallow)
+                return False
+
+        return len(stack) == 0
