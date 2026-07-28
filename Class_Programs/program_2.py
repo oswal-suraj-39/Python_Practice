37b96bc1997bc3c26e8b5d95490db25f307e8ea1
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-28 18:59:38 +0530

    add: create class to validate string of parentheses

---
+++ Class_Programs/program_2.py
@@ -0,0 +1,3 @@
+# Write a Python class to find validity of a string of parentheses, '(', ')', '{', '}', '['  ']’. 
+# These brackets must be close in the correct order. for example "()" and "()[]{}" are valid 
+# but "[)", "({[)]" and "{{{" are invalid. i maybe understand
