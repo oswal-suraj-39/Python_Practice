f49f83da7686bacd811f4dca16082d94a15e59b3
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-28 19:05:53 +0530

    add tuple definitions for element-wise sum calculation

--- Tuple_Programs/program_2.py
+++ Tuple_Programs/program_2.py
@@ -1,3 +1,8 @@
 # Write a Python program to compute element-wise sum of given tuples.   
 # Original lists: (1, 2, 3, 4) (3, 5, 2, 1) (2, 2, 3, 1) 
-# Element-wise sum of the said tuples: (6, 9, 8, 6) 
+# Element-wise sum of the said tuples: (6, 9, 8, 6)
+
+t1 = (1, 2, 3, 4)
+t2 = (3, 5, 2, 1)
+t3 = (2, 2, 3, 1)
+l1 = []
