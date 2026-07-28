64448df290c242cd7dcf87d68b766fd47bbbb8cd
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-28 19:02:54 +0530

    add input statement, execute class, check validation, and print result

--- Class_Programs/program_2.py
+++ Class_Programs/program_2.py
@@ -11,14 +11,21 @@ class BracketValidator:
         bracket_map = {')': '(', '}': '{', ']': '['}
 
         for char in self.string:
-            if char in bracket_map.values():  # opening brackets
+            if char in bracket_map.values():
                 stack.append(char)
-            elif char in bracket_map:  # closing brackets
+            elif char in bracket_map:  
                 if not stack or stack[-1] != bracket_map[char]:
                     return False
                 stack.pop()
             else:
-                # Invalid character (optional to allow or disallow)
                 return False
 
         return len(stack) == 0
+    
+input_str = input("Enter a string of brackets: ")
+validator = BracketValidator(input_str)
+
+if validator.isValid():
+    print("Valid string")
+else:
+    print("Invalid string")
