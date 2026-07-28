1bb3b99dc8411eb7d816c5f66200b3405c5f73f8
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-28 18:55:25 +0530

    add: implement class to reverse a string

--- String_Programs/program_3.py
+++ String_Programs/program_3.py
@@ -1,1 +1,9 @@
-# Write a Python script using class to reverse a string word by word 
+# Write a Python script using class to reverse a string word by word
+
+class String:
+    def reverse_string(self):
+        self.string = input("enter a string: ")
+        print(self.string[::-1])
+        
+s = String()
+s.reverse_string()
