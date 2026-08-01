bf70f127fe611643fbb4f7ceba391724ffe3e458
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-01 13:43:12 +0530

    add: implement Rectangle class with area and perimeter methods

--- Class_Programs/program_3.py
+++ Class_Programs/program_3.py
@@ -1,2 +1,16 @@
 # Write a python script to create a class Rectangle with data member’s length, width 
 # and methods area, perimeter which can compute the area and perimeter of rectangle. 
+
+class rectangle:
+    length = 4
+    width = 4
+
+    def area(self):
+        result1 = self.length * self.width
+        return result1
+    
+    def perimeter(self):
+        result2 = 2 * (self.length + self.width)
+        return result2
+    
+rect = rectangle()
