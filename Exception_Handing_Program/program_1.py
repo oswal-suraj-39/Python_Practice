3d0fb02989f22d30959f6477da3549b57c1372d2
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-02 22:54:58 +0530

    add: create Date class with user input and exception handling

---
+++ Exception_Handing_Program/program_1.py
@@ -0,0 +1,4 @@
+# Define a class Date (Day, Month, Year) with functions to accept and display it. 
+# Accept date from user. 
+# Throw user defined exception “invalid Date Exception” 
+# if the date is invalid.
