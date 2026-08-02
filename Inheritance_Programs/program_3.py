8da0dff2706d25238fad491e761fa69b42852572
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-02 23:17:19 +0530

    add: implement calculate methods for Square and Circle classes

--- Inheritance_Programs/program_3.py
+++ Inheritance_Programs/program_3.py
@@ -10,14 +10,25 @@ class square(shape):
     def __init__(self, l, r):
         self.length = l
         self.radius = r
+        
+    def calculate(self):
+        print(f"area of square: {self.length ** 2}")
+        print(f"volume of square: {self.length ** 3}")
 
 class circle(shape):
     def __init__(self, l, r):
         self.length = l
         self.radius = r
+        
+    def calculate(self):
+        print(f"area of circle: {3.14 * self.radius ** 2}")
+        print(f"volume of circle: {4/3 * 3.14 * self.radius ** 3}")
 
 l = int(input("enter a length: "))
 r = int(input("enter a radius: "))
 
 squ = square(l, r)
 cir = circle(l,r)
+
+squ.calculate()
+cir.calculate()
