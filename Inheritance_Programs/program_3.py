20d0a2e21b3f26ff8cc4cd96c6cdcb3d8c324261
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-02 23:15:32 +0530

    add: implement Shape class and its subclasses with initialization for length and radius

--- Inheritance_Programs/program_3.py
+++ Inheritance_Programs/program_3.py
@@ -1,3 +1,23 @@
 # Define a class named Shape and its subclass(Square/ Circle).
 # The subclass has an init function which takes an argument (Length/radius).
 # Both classes should have methods to calculate area and volume of a given shape.
+
+class shape:
+    length = 0
+    radius = 0
+
+class square(shape):
+    def __init__(self, l, r):
+        self.length = l
+        self.radius = r
+
+class circle(shape):
+    def __init__(self, l, r):
+        self.length = l
+        self.radius = r
+
+l = int(input("enter a length: "))
+r = int(input("enter a radius: "))
+
+squ = square(l, r)
+cir = circle(l,r)
