5a2849e356c50f21b0ae3491b6661543421a0cdc
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-02 22:56:09 +0530

    add: define Date class with default values and display method

--- Exception_Handing_Program/program_1.py
+++ Exception_Handing_Program/program_1.py
@@ -2,3 +2,13 @@
 # Accept date from user. 
 # Throw user defined exception “invalid Date Exception” 
 # if the date is invalid.
+
+import datetime as dt
+
+class date:
+    day = 1
+    month = 1
+    year = 2000
+
+    def display (self):
+        print(dt.datetime(self.year, self.month, self.day))
