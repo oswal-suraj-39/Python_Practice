4a966a898f94e5d337c459a5066f4a63e798e4f0
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-02 23:07:32 +0530

    initial: question define Shape class and its subclasses with area and volume methods

---
+++ Inheritance_Programs/program_3.py
@@ -0,0 +1,3 @@
+# Define a class named Shape and its subclass(Square/ Circle).
+# The subclass has an init function which takes an argument (Length/radius).
+# Both classes should have methods to calculate area and volume of a given shape.
