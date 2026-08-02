5ecc21770fe0bf8b498329df76988d2138b51846
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-02 23:45:48 +0530

    add: implement bubble sort algorithm to sort a list

--- List_Programs/program_4.py
+++ List_Programs/program_4.py
@@ -3,3 +3,13 @@
 li = [9, 5, 7, 3, 8, 2]
 
 print("unsorted list: ", li)
+
+for j in range(len(li)):
+    
+    for i in range(0, len(li) - 1):
+        if li[i] > li[i+1]:
+            temp = li[i]
+            li[i] = li[i+1]
+            li[i+1] = temp
+               
+print("sorted list: ", li)
