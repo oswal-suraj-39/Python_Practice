a629e173de851dcfb0a14ace889a57059709d9b3
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-05 19:54:25 +0530

    add: enhance program to calculate and display areas of square and rectangle

--- Function_Programs/program_2.py
+++ Function_Programs/program_2.py
@@ -2,3 +2,10 @@
 
 square_area = lambda side : side * side
 rectangle_area = lambda length , width : length * width
+
+s = int(input("enter a side: "))
+l = int(input("enter a length: "))
+w = int(input("enter a width: "))
+
+print("area of square: ",square_area(s))
+print("area of rectangle: ",rectangle_area(l, w))
