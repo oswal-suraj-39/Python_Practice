7f2e571ea23e799f8146e5ed45dc2f341e3880b2
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-07 23:43:10 +0530

    add: complete implementation of merging two lists into a list of tuples

--- List_Programs/program_6.py
+++ List_Programs/program_6.py
@@ -5,3 +5,16 @@ list2 = []
 list3 = []
 
 n = int(input("how many element: "))
+
+for i in range(n):
+    e = int(input("enter num: "))
+    list1.append(e)
+
+for i in range(n):
+    e = input("enter char: ")
+    list2.append(e)
+
+for i in range(n):
+    list3.append((list1[i], list2[i]))
+
+print(list3)
