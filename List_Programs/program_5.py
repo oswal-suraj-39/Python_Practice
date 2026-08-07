823d15dedeb69f45319c61955b137bc591b66561
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-07 23:39:29 +0530

    add: implement logic to unzip a list of tuples into individual lists

--- List_Programs/program_5.py
+++ List_Programs/program_5.py
@@ -4,3 +4,10 @@ zipped_list = [(1, 'a'), (2, 'b'), (3, 'c')]
 
 list1 = []
 list2 = []
+
+for i in zipped_list:
+    list1.append(i[0])
+    list2.append(i[1])
+
+print("List 1:", list1)
+print("List 2:", list2)
