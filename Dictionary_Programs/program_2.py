17bbfd9e752c81df340e0bd3b01aca7bc89a6533
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:15:43 +0530

    add: enhance dictionary sorting script with complete sorting functionality by key and value

--- Dictionary_Programs/program_2.py
+++ Dictionary_Programs/program_2.py
@@ -1,3 +1,11 @@
 # Write a Python script to sort (ascending and descending) a dictionary by key and value.
 
 dict1 = {'b': 3, 'a': 1, 'd': 4, 'c': 2}
+
+print("ascending order by key: ",sorted(dict1.items()))
+
+print("descending order by key: ",sorted(dict1.items(), reverse="True"))
+
+print("ascending order by value: ",sorted(dict1.items(), key=lambda item: item[1]))
+
+print("descending order by value: ",sorted(dict1.items(), reverse="True", key=lambda item: item[1]))
