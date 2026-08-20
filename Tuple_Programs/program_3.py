ffb92a1c4e67e951bbb1261bb30b96d0f6676dcd
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:39:53 +0530

    add: implement program to convert tuple of string values to tuple of integer values

---
+++ Tuple_Programs/program_3.py
@@ -0,0 +1,8 @@
+# Write a Python program to convert a tuple of string values to a tuple of integer 
+# values.   Original tuple values: (('333', '33'), ('1416', '55')) 
+#           New tuple values:      ((333, 33), (1416, 55))
+
+list1 = []
+tuple1 = ("10", "20", "30", "40", "50")
+
+print("Original tuple values: ", tuple1)
