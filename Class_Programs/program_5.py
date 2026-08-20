8fa9735571bb00e7bcdec9bcce47b9f2869725f2
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:21:02 +0530

    initial: implement Student class with attributes for name and marks

---
+++ Class_Programs/program_5.py
@@ -0,0 +1,7 @@
+# Write a Python class named Student with two attributes student_name, marks. 
+# Modify the attribute values of the said class and 
+# print the original and modified values of the said attributes. 
+
+class student:
+    student_name = "suraj"
+    marks = 90
