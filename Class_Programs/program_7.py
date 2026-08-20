9259f8cf0f0e667ad1b3c281d2c53e543cafde63
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:44:04 +0530

    initial: implement basic calculator class with initialization for two numbers

---
+++ Class_Programs/program_7.py
@@ -0,0 +1,6 @@
+# Write a Python script to Create a Class which Performs Basic Calculator Operations.
+
+class calculate:
+    def __init__ (self, num1, num2):        
+        self.num1 = num1
+        self.num2 = num2
