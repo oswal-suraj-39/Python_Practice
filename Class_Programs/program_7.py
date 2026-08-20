5b6dac0951cc705b9e66c91a17320b5e587bf54b
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:45:05 +0530

    add: implement basic calculator operations in calculate class

--- Class_Programs/program_7.py
+++ Class_Programs/program_7.py
@@ -4,3 +4,25 @@ class calculate:
     def __init__ (self, num1, num2):        
         self.num1 = num1
         self.num2 = num2
+        
+    def add(self):
+        print(self.num1 + self.num2)
+
+    def sub(self):
+        print(self.num1 - self.num2)
+
+    def mul(self):
+        print(self.num1 * self.num2)
+
+    def div(self):
+        print(self.num1 / self.num2)
+
+num1 = int(input("enter number1 : "))
+num2 = int(input("enter number2 : "))
+
+cal = calculate(num1, num2)
+
+cal.add()
+cal.sub()
+cal.mul()
+cal.div()
