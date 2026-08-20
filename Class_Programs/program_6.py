59539d9a4ae843e5b20d7eb55e521e3a8a4b1f99
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:28:20 +0530

    add: implement Circle class to compute area and circumferences using parameterized constructor

--- Class_Programs/program_6.py
+++ Class_Programs/program_6.py
@@ -5,3 +5,20 @@ class circle:
     
     area = 0
     circumferences = 0
+    
+    def __init__(self, r):
+        self.radius = r
+        self.area = 0
+        self.circumferences = 0
+
+    def compute(self):
+        self.area = 3.14 * self.radius**2
+        self.circumferences = 2 * 3.14 * self.radius
+
+r = int(input("enter a radius: "))    
+c = circle(r)
+
+c.compute()
+
+print("area of circle: ", c.area)
+print("circumferences of circle: ",c.circumferences)
