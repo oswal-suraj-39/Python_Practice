d0cb2168b70e4c979ba7c6b6f863edf5562199dd
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:27:38 +0530

    initial: create Circle class to compute area and circumferences using parameterized constructor

---
+++ Class_Programs/program_6.py
@@ -0,0 +1,7 @@
+# Write a python program to create a class Circle and Compute the Area 
+# and the circumferences of the circle.(use parameterized constructor) 
+
+class circle:
+    
+    area = 0
+    circumferences = 0
