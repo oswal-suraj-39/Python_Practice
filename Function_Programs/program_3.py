c36b148c3b243ebbd7883adfb7312ef4c522e7eb
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-20 17:25:36 +0530

    add: implement function to remove characters with odd index values from a string

--- Function_Programs/program_3.py
+++ Function_Programs/program_3.py
@@ -1,3 +1,17 @@
 # Write a python program to accept string and 
 # remove the characters which have odd index values of given string using user defined function.
 
+def removeOddIndexValues(string):
+    
+    result = ""
+    
+    for i in range(len(string)):
+        
+        if i % 2 == 0:
+            result += string[i]
+            
+    print(result)
+
+string = input("enter a string: ")
+
+removeOddIndexValues(string)
