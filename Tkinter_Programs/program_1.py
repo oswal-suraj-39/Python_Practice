798acab7670cedd4f43ee254ac6aed425c335d77
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-27 22:48:18 +0530

    add: enhance age calculator GUI with input fields for day, month, and year

--- Tkinter_Programs/program_1.py
+++ Tkinter_Programs/program_1.py
@@ -7,4 +7,15 @@ top = Tk()
 top.geometry("300x200")
 top.title("Age Calculator")
 
+label1 = Label(top, text="Enter your Day:")
+day = Entry(top)
+
+label2 = Label(top, text="Enter your Month:")
+month = Entry(top)
+
+label3 = Label(top, text="Enter your Year:")
+year = Entry(top)
+
+button = Button(top, text="Calculate Age", command=calculate_age)
+
 top.mainloop()
