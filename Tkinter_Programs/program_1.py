24e3e22434ce95b6450f1ee67b7b57d7c24b7abb
oswal-suraj-39 <surajoswal39@gmail.com>
2026-08-27 22:47:31 +0530

    add: implement basic GUI structure for age calculator application

--- Tkinter_Programs/program_1.py
+++ Tkinter_Programs/program_1.py
@@ -1,2 +1,10 @@
 # Write Python GUI program to take accept your birthdate 
 # and output your age when a button is pressed.
+
+from tkinter import Tk, Label, Entry, Button
+
+top = Tk()
+top.geometry("300x200")
+top.title("Age Calculator")
+
+top.mainloop()
